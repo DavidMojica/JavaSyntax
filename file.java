d93944140e4462417842515bff57df754c2608fe
David Mojica <davidmojicav@gmail.com>
2024-02-08 22:08:26 -0500

    x

--- file.java
+++ file.java
@@ -5,14 +5,19 @@ public class file {
         byte b = -128; //Range: -128 to 127 | 8 bits
         short s = -32768; //Range: -32768 to 32767 | 16 bits
         int numI = 29932938; //Range: -2,147,483,648 to 2,147,483,647 | 32 bits
-        long numL = 3238118123382318312L; //Range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 with 'L' at the end.
+        long numL = 3238118123382318312L; //Range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 with 'L' at the end. | 64 Bits
 
         // Floatings
-        
+        float numF = 3.2123234f; //Range: 32 bits with 'F' at the end.
+        double numD = 8327.2381;
 
+        // Char
+        char c = 'v';
 
+        //Bool
+        boolean ban = true;
 
-
-
+        //----------Arrays------------//
+        //import java.util.LinkedList;
     }
 }
