765677db616f9357882fe35937b0d41c26b89cf2
David Mojica <davidmojicav@gmail.com>
2024-02-08 21:57:24 -0500

    Init

---
+++ file.java
@@ -0,0 +1,18 @@
+public class file {
+    public static void main(String[] args){
+        //----------Variables-----------//
+        // Integers
+        byte b = -128; //Range: -128 to 127 | 8 bits
+        short s = -32768; //Range: -32768 to 32767 | 16 bits
+        int numI = 29932938; //Range: -2,147,483,648 to 2,147,483,647 | 32 bits
+        long numL = 3238118123382318312L; //Range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 with 'L' at the end.
+
+        // Floatings
+        
+
+
+
+
+
+    }
+}
